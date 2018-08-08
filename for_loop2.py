9c4d0f02a354a374f0e207fcb865a5edf5fe0069
Pratiksha <prati.vaishnav16@gmail.com>
2018-08-08 17:45:20 +0530

    [ADD]Added comments for for_loop understandig the program.

--- for_loop2.py
+++ for_loop2.py
@@ -1,9 +1,14 @@
+#This is for Loop pttern program which print numeric pyramid
+ 
 r= int(input("enter the raw: "))
 
 for i in range(r,0,-1):
     for j in range(1,i):
         print(j,end=" ")
     print()
+
+#this is 2nd For Loop program
+ 
 r= int(input("enter the raw: "))
 
 for i in range(r,0,-1):
