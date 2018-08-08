c4f1a6f44aa1bf7f0c25086d711e3267b18bac41
Pratiksha <prati.vaishnav16@gmail.com>
2018-08-08 17:27:49 +0530

    [ADD]Added files to newly created repo.

---
+++ forLoop1.py
@@ -0,0 +1,18 @@
+r = int(input("enter the raw :"))
+
+
+for i in range(0,r):
+
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+r = int(input("enter the raw :"))
+
+for i in range(0,r):
+
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+exit()
---
+++ for_loop2.py
@@ -0,0 +1,15 @@
+r= int(input("enter the raw: "))
+
+for i in range(r,0,-1):
+    for j in range(1,i):
+        print(j,end=" ")
+    print()
+r= int(input("enter the raw: "))
+
+for i in range(r,0,-1):
+    for j in range(i,0,-1):
+        print(j,end=" ")
+    print()
+
+
+exit()
